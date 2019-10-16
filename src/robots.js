9b7b9beb7927f63aae4b651f4fe70d9e2efe4d47
viditchaurasia <vidit.chaurasia@tenzing.co.nz>
2019-10-16 14:56:25 +1300

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leane Graham',
+	username: 'Bret',
+	email: 'since@april.biz'
+},
+{
+	id: 2,
+	name: 'Ervin Howell',
+	username: 'Antnotette',
+	email: 'erlas@april.tv'
+},
+{
+	id: 3,
+	name: 'Clementine Savour',
+	username: 'Sandst',
+	email: 'label@jan.liz'
+},
+{
+	id: 4,
+	name: 'Patricia Lorez',
+	username: 'Chuck',
+	email: 'check@lorex.biz'
+},
+{
+	id: 5,
+	name: 'Toer Limba',
+	username: 'Lorem',
+	email: 'colon@loren.tz'
+},
+{
+	id: 6,
+	name: 'Synical Tormento',
+	username: 'Migz',
+	email: 'sym@toremz.tz'
+},
+{
+	id: 7,
+	name: 'lillian Puttinson',
+	username: 'Lame',
+	email: 'cane@puting.biz'
+},
+{
+	id: 8,
+	name: 'Tim Burton',
+	username: 'Burty',
+	email: 'lip@cert.tv'
+},
+{
+	id: 9,
+	name: 'Beany Coolio',
+	username: 'Looli',
+	email: 'milia@silly.tv'
+},
+{
+	id: 10,
+	name: 'Chafeur Shapey',
+	username: 'Cefus',
+	email: 'cips@lulo.tz'
+}
+]
