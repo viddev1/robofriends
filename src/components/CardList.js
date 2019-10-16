0d8ed21a4aff0e1c38c8f3ae4f0aa55a9aa6deba
viditchaurasia <vidit.chaurasia@tenzing.co.nz>
2019-10-16 15:06:29 +1300

    commented error code

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,9 +3,9 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
 
-	if (true) {
-		throw new Error('Nooooo!');
-	}
+	// if (true) {
+	// 	throw new Error('Nooooo!');
+	// }
 
 	const cardComponent = robots.map((user, i) => {
 		return (
