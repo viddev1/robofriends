1b3f3dcc6a6d305b6eed3a4507d9802f5bfc5ed6
viditchaurasia <vidit.chaurasia@tenzing.co.nz>
2019-10-16 15:01:35 +1300

    Updates

---
+++ precache-manifest.54132966300de1cb33fbd9f4e9634aef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb327ab3740042dfd317fe5dfc96089",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4235c839ea2cd4aa0074",
+    "url": "/robofriends/static/css/main.6e117b5b.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "4235c839ea2cd4aa0074",
+    "url": "/robofriends/static/js/main.601bab63.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
